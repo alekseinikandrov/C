f87d98cf28b5199806e866f2cc148577acb70de5
Aleksei <aleksei.nikandrov@gmail.com>
2013-10-15 16:38:46 +0300

    Update and rename korrutus.c to ruut.c

--- ruut.c
+++ ruut.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (int argc, char *argv[]) {
-	printf("\n\t Korrutus tabel \n");	
-	int a;
+	printf("\n\t Ruutude tabel \n");	
+	int a; 
 	for(a=1;a<21;a++){
 		printf("%d %d\n", a, a*a);
 	}
