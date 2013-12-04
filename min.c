a61768a6e536ab9b9ab577a472918402562d18dc
Aleksei <aleksei.nikandrov@gmail.com>
2013-12-04 17:05:34 +0200

    2 arvu min

---
+++ min.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int min (int a, int b)
+{
+    if (a < b)
+    {
+        return a;
+    }
+    return b;
+}
+
+int main(int argc, char *argv[])
+{
+    int c = 10;
+
+    {
+        printf("%d ja 5 <=%d\n", c, min(c, 5));
+    }
+    return 0;
+}
