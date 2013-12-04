8265744c5f246e83aa7b73fae26e0bfc2855e81c
Aleksei <aleksei.nikandrov@gmail.com>
2013-12-04 17:07:14 +0200

    2 arvu max

---
+++ max.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int min (int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    return b;
+}
+
+int main(int argc, char *argv[])
+{
+    int c = 12;
+
+    {
+        printf("%d ja 6 >=%d\n", c, min(c, 6));
+    }
+    return 0;
+}
