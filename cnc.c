5df5796933df3caf7f5c118af6146f3d1b58491d
Aleksei <aleksei.nikandrov@gmail.com>
2013-12-12 10:58:55 +0200

    CNC code...

--- cnc.c
+++ cnc.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-float gcode(float x, float y, float w, float h){
-	printf("G X GoTo %f\n", x);
-	printf("G Y GoTo %f\n", y);
-	printf("G Z Down %f\n");
-	printf("G X Move %f\n", w);
-	printf("G Y Move %f\n", h);
-	printf("G X Move %f\n", -w);
-	printf("G Y Move %f\n", -h);
-	printf("G Z Up   %f\n");
+void gcode(float x, float y, float w, float h){
+        printf("G X GoTo %f\n", x);
+        printf("G Y GoTo %f\n", y);
+        printf("G Z Down\n");
+        printf("G X Move %f\n", w);
+        printf("G Y Move %f\n", h);
+        printf("G X Move %f\n", -w);
+        printf("G Y Move %f\n", -h);
+        printf("G Z Up\n");
 }
 
 int main(int argc, char *argv[]){
-	gcode(100, 200, 44, 33);
-	return 0;
+        gcode(100, 200, 44, 33);
+        return 0;
 }
