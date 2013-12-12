c754dd9a05998d25e055f8619cb223b79a6a8fae
Aleksei <aleksei.nikandrov@gmail.com>
2013-12-12 11:58:39 +0200

    koolis

--- cnccode.c
+++ cnccode.c
@@ -8,18 +8,43 @@ void gcode(float x, float y, float w, float h){
         printf("G Y Move %f\n", h);
         printf("G X Move %f\n", -w);
         printf("G Y Move %f\n", -h);
-        printf("G Z Up\n");
+        printf("G Z Up\n\n");
 }
 
-void cnc(float y, int n){
-		float x;
-		int i;
-		for(i=0; i<(n); i++){
-			gcode(145, y, 44, 33);
-		}
+void cnc(float x, float y, float w, float h, float dx,int n){
+      
+        int i;
+        for(i = 0; i < n; i++){
+			gcode(x, y, w, h);
+			x = x + w + dx;
+        }
 }
 
 int main(int argc, char *argv[]){
-        cnc(200, 6);
+        cnc(10, 200, 44, 33, 1, 5);
+        return 0;
+}
+#include <stdio.h>
+
+void gcode(float x, float y, float w, float h){
+        printf("G X GoTo %f\n", x);
+        printf("G Y GoTo %f\n", y);
+        printf("G Z Down\n");
+        printf("G X Move %f\n", w);
+        printf("G Y Move %f\n", h);
+        printf("G X Move %f\n", -w);
+        printf("G Y Move %f\n", -h);
+        printf("G Z Up\n\n");
+}
+
+void cnc(float x0, float x1, float y, float w, float h, float dx){
+		float x;
+        for(x = x0; x + w < x1; x = x + w + dx){
+			gcode(x, y, w, h);
+        }
+}
+
+int main(int argc, char *argv[]){
+        cnc(10, 100, 200, 44, 33, 1);
         return 0;
 }
